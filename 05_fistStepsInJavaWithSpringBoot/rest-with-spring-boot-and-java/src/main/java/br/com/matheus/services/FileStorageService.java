f3fdab340af7e1f71fc05eacfbfe1850c0510dda
Matheus Peixoto Matos <roanda18@gmail.com>
2025-02-18 16:36:08 -0300

    Criando serviço responsavel por armazernar os arquivos em disco

---
+++ 05_fistStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/matheus/services/FileStorageService.java
@@ -0,0 +1,49 @@
+package br.com.matheus.services;
+
+import br.com.matheus.config.FileStorageConfig;
+import br.com.matheus.exceptions.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Cold not create the directory where the uploaded files will be stored ", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+
+        } catch (Exception ex) {
+
+            throw new FileStorageException("Cold not store file " + fileName + ". Please try again", ex);
+        }
+    }
+}
