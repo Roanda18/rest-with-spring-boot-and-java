f349e39b07834656151c96447b43f110b13e8ee0
Matheus Peixoto Matos <roanda18@gmail.com>
2025-02-20 10:47:48 -0300

    Concluido a adição do endpoint de download de arquivos.

--- 05_fistStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/matheus/controllers/FileStorageController.java
+++ 05_fistStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/matheus/controllers/FileStorageController.java
@@ -1,15 +1,15 @@
 package br.com.matheus.controllers;
 
-import br.com.matheus.exceptions.MyFileNotFoundException;
 import br.com.matheus.services.FileStorageService;
 import br.com.matheus.vo.v1.UploadFileResponseVO;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -42,7 +42,7 @@ public class FileStorageController {
     }
 
     @PostMapping("/uploadMultipleFiles")
-    public List <UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
 
         logger.info("Storing files to disk");
 
@@ -50,6 +50,30 @@ public class FileStorageController {
                 .stream()
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+
+        logger.info("Reading a file on disk");
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type! ");
+        }
+
+        if (contentType.isBlank()) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
 
